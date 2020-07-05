9fdb5d5323e86290527ec1a18f3bc48cce2c6100
sanjidtk <48551302+sanjidtk@users.noreply.github.com>
2020-07-05 09:27:36 +0530

    Add files via upload

---
+++ bot.php
@@ -0,0 +1,11 @@
+<?php
+error_reporting(0);
+
+DEFINE('ROOTDIR', __DIR__);
+require ROOTDIR . '/src/autoloader.php';
+$loader = new autoloader();
+$loader->addNamespace('xosad', ROOTDIR . '/src/xosad');
+$loader->register();
+use xosad\igFame;
+$i = new igFame();
+$i->startCore();
